b5d2e5feab3d043425d79753e2a2d428abc32459
Richard Liu <richard.y.liu@berkeley.edu>
2020-10-03 22:38:35 -0700

    Finished integration

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
 import React from 'react';
 import Row from './Row';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [veggies, setVeggies] = React.useState(null);
+  async function fetchData() {
+    const res = await axios.get(
+      'https://api.airtable.com/v0/appPnOcQjOnX4INQH/Vegetables',
+      {
+        headers: {
+          authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+        },
+      }
+    );
+    console.log(res.data);
+    setVeggies(res.data.records);
+  }
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+  if (!veggies) {
+    return <div>Loading...</div>;
+  }
   return (
     <div>
       <h1>The Definitive Vegetable Ranking</h1>
       <table>
-        <tr>
-          <th>Name</th>
-          <th>Notes</th>
-          <th>Rating</th>
-        </tr>
-        <Row
-          info={{
-            name: 'Cabbage 🥬',
-            notes: 'Needs to be seasoned well to taste good',
-            rating: 3,
-          }}
-        />
-        <Row
-          info={{
-            name: 'Mushroom 🍄',
-            notes: 'Very solid',
-            rating: 8,
-          }}
-        />
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Notes</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          {veggies.map((record) => (
+            <Row key={record.id} info={record.fields} />
+          ))}
+        </tbody>
       </table>
     </div>
   );
